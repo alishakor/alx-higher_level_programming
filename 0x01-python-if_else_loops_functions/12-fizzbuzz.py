ee955034acfb91e6c15e192cb01b0b24a300c587
alishakor <aliuokeowo@gmail.com>
2023-01-05 16:14:15 +0100

    a function that print FIZZBUZZ for multiples of 3 and 5

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for num in range(1, 101):
         if num % 3 == 0 and num % 5 == 0
-            print("FIZZBUZZ", end=" ")
+            print("FizzBuzz", end=" ")
         elif num % 3 == 0:
-            print("FIZZ", end=" ")
+            print("Fizz", end=" ")
         elif num % 5 == 0:
-            print("BUZZ", end=" ")
+            print("Buzz", end=" ")
         else:
             print(f"{num}", end=" ")
