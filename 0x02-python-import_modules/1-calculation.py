c5bdc8e7eb7d23e2f34fde8a6fec98bcf6a25ea0
alishakor <aliuokeowo@gmail.com>
2023-01-10 19:32:04 +0100

    a function that calculate the basic math operations

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    return ("{} + {} = {}".format(a, b, add(a,b)))
-    return ("{} - {} = {}".format(a, b, sub(a,b)))
-    return ("{} * {} = {}".format(a, b, mul(a,b)))
-    return ("{} / {} = {}".format(a, b, div(a,b)))
+    print ("{} + {} = {}".format(a, b, add(a, b)))
+    print ("{} - {} = {}".format(a, b, sub(a, b)))
+    print ("{} * {} = {}".format(a, b, mul(a, b)))
+    print ("{} / {} = {}".format(a, b, div(a, b)))
