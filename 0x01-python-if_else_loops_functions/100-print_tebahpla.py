b9bc31de231d795e07a95e574964e74223c0e86f
alishakor <aliuokeowo@gmail.com>
2023-01-10 10:30:26 +0100

    printing alphabet in reverse order with changing cases

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for character in range(122, 96, -1):
-    if character%2 == 0:
+for character in range(ord('z'), ord('a') - 1, -1):
+    if character % 2 == 0:
         character = chr(character)
     else:
         character = chr(character - 32)
