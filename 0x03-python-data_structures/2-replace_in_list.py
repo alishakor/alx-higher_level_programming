f2c3934ac27f08e65b17154a502a80a3c32be3be
alishakor <aliuokeowo@gmail.com>
2023-01-11 02:37:29 +0100

    a program that replaces an element of a list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,6 +2,5 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0 and idx > len(my_list):
         return my_list
-    else:
-        mylist[idx] = element
-        return my_list
+    mylist[idx] = element
+    return my_list
