1332c6c9ed6c35ff669374d962d44bfafc627fae
alishakor <aliuokeowo@gmail.com>
2023-01-11 01:05:41 +0100

    a program that imports a variable from a file

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,5 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 if __name__ == "__main__":
-    from variable_load_5 import a
+    import variable_load_5 as var
 
-    print("{}".format(a))
+    print(var.a)
