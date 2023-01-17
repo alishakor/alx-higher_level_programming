a2eaa6674ffc3c0b6e8ab79a419d8f63ceaaaf36
alishakor <aliuokeowo@gmail.com>
2023-01-17 08:18:38 +0100

    function that prints sorted dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sort_a = sorted(a_dictionary):
-    for i in sort_a:
+    for i in sorted(a_dictionary):
         print(f"{i}: {a_dictionary[i]}")
