0d63a0c8918542184c15fa38041a301cbba8fd98
alishakor <aliuokeowo@gmail.com>
2023-01-24 18:50:32 +0100

    a class size with private instance attritube

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""define a classs square"""
+
+
+class Square:
+    """class called square"""
+
+    def __init__(self, size):
+        """initialize a new square
+
+        Args:
+            size (int): size of square
+        """
+
+        self.__size = size
