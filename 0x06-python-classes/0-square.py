ecdc5700e920993ba6dec4e9b5102d48799c7696
alishakor <aliuokeowo@gmail.com>
2023-01-24 18:28:38 +0100

    an empty class that defines a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""an empty class Square that defines a square"""
+"""Square - an empty class that defines a square"""
+
 
 class Square:
     """empty class that defines a square"""
