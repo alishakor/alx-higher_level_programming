f74a65355da28fda741b2210d7db4ed73aad0941
alishakor <aliuokeowo@gmail.com>
2023-02-07 15:25:52 +0100

    Read from a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""0-read_file.py module"""
+
+
+def read_file(filename=""):
+    """a function that reads a text file (UTF8) and prints it to stdout
+
+    Args:
+        filename: The file to be read
+
+    """
+
+    with open('filename', encoding="utf-8") as f:
+        read_content = f.read()
+        print(read_content, end="")
