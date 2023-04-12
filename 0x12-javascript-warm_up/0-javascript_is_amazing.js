0e7cbe401e8c1160778c0bc4502f96e3381439e7
alishakor <113800450+alishakor@users.noreply.github.com>
2023-04-12 05:39:21 +0100

    task 0

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 
-// script that print console.log
+// a script that prints “JavaScript is amazing”:
+// You must create a constant variable called myVar with the value “JavaScript is amazing”
+// You must use console.log(...) to print all output
+// You are not allowed to use var
 
-const myvar = 'JavaScript is amazing';
-console.log(myvar);
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
