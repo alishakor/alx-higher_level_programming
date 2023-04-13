e4faa0cc94d8a95910f1f9bd98b799675f607706
alishakor <aliuokeowo@gmail.com>
2023-04-13 05:47:48 +0100

    class rectangle with methods

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+// A class "Rectangle" that defines a rectangle
+// You must use the class notation for defining your class
+// The constructor must take 2 arguments: w and h
+// Initialize the instance attribute width with the value of w
+// Initialize the instance attribute height with the value of h
+// If w or h is equal to 0 or not a positive integer, create an empty object
+// Create an instance method called print() that prints the rectangle using the character X
+
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let j = 0; j < this.height; k++) {
+      let result = '';
+      for (let k = 0; k < this.width; k++) {
+        result += 'X';
+      }
+      console.log(result);
+    }
+  }
+}
+
+module.exports = Rectangle;
