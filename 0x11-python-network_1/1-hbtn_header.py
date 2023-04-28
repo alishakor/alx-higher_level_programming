5fcd20aa8fb1d58534985c612cda314eaff69fb7
alishakor <aliuokeowo@gmail.com>
2023-04-28 23:46:55 +0100

    a script that takes a url, send request and displays the value of X-Request-Id

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" a Python script that takes in a URL and an email, sends a POST
+    request to the passed URL with the email as a parameter, and
+    displays the body of the response (decoded in utf-8)
+    You must use the packages urllib and sys
+    You are not allow to import packages other than urllib and sys
+    The value of this variable is different for each request
+    You don’t need to check arguments passed to the script (number or type)
+    You must use a "with" statement
+"""
+import sys
+import urllib.request
+
+
+if _name_ == "_main_":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as feedback:
+        headers = feedback.info()
+        print(headers['X-Request-Id'])
