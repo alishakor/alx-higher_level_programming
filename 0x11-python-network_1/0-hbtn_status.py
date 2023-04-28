9ead94c58f972499308721bf6c4a6005c721c84c
alishakor <aliuokeowo@gmail.com>
2023-04-28 23:35:36 +0100

    a script that fetches a url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" a Python script that fetches https://alx-intranet.hbtn.io/status
+    You must use the package urllib
+    You are not allowed to import any packages other than urllib
+    The body of the response must be displayed like the following
+    example (tabulation before -)
+    You must use a with statement
+"""
+import urllib.request
+
+
+if _name_ == "_main_":
+    with urllib.request.urlopen(
+                'https://alx-intranet.hbtn.io/status') as feedback:
+        html = feedback.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
