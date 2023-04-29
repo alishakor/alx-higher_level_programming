49ce7a43a9ba221864007f25359fe1a6c0209f1c
alishakor <aliuokeowo@gmail.com>
2023-04-29 03:44:46 +0100

    script that takes in a letter and sends a POST request

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter.
+
+The letter must be sent in the variable q
+If no argument is given, set q=""
+If the response body is properly JSON formatted and not empty,
+display the id and name like this: [<id>] <name>
+Otherwise:
+Display Not a valid JSON if the JSON is invalid
+Display No result if the JSON is empty
+You must use the package requests and sys
+You are not allowed to import packages other than requests and sys
+"""
+
+import requests
+import sys
+
+
+if _name_ == "_main_":
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    url = 'http://0.0.0.0:5000/search_user'
+    info = {'q': q}
+
+    response = requests.post(url, info)
+
+    try:
+        json_info = response.json()
+
+        if json_info:
+            print("[{}] {}".format(json_info['id'],
+                                   json_info['name']))
+        else:
+            print("No result")
+
+    except ValueError:
+        print("Not a valid JSON")
