748240c7fcb0ec784e6f5ded204bf44a90fdaf0c
alishakor <aliuokeowo@gmail.com>
2023-05-23 23:44:55 +0100

    checking semi-standard

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// Write a script that reads and prints the content of a file.
+// Write a script that i if a fil.
 // The first argument is the file path
 // The content of the file must be read in utf-8
 // If an error occurred during the reading, print the error object
@@ -11,6 +11,6 @@ fs.readFile(arg, 'utf8', (error, content) => {
   if (error) {
     console.error(error);
   } else {
-    console.log(content);
+      console.log(content);
   }
 });
