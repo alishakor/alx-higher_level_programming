198772dd2693a6340fb61f05fe9b624a378a4c49
alishakor <aliuokeowo@gmail.com>
2023-05-23 23:41:45 +0100

    script that read and print file content

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// Write a script that reads and prints the content of a file.
+// The first argument is the file path
+// The content of the file must be read in utf-8
+// If an error occurred during the reading, print the error object
+const fs = require('fs');
+
+const arg = process.argv[2];
+
+fs.readFile(arg, 'utf8', (error, content) => {
+  if (error) {
+    console.error(error);
+  } else {
+      console.log(content);
+  }
+});
