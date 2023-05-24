55a13cf27a39f225fbcab05062b2499312f7332c
alishakor <aliuokeowo@gmail.com>
2023-05-24 05:47:10 +0100

    advanced task

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+// Write a script that computes the number of tasks completed by user id.
+// The first argument is the API URL: https://jsonplaceholder.typicode.com/todos
+// Only print users with completed task
+// You must use the module request
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
+      }
+    }
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
+});
